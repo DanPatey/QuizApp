b17d3fa0b020a66329e47f201f7aa12384fbc21b
Dan Patey <danpatey@danpatey.com>
2016-06-30 22:04:47 -0700

    Fun with some elementary animation!

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -9,40 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var currentQuestionLabel: UILabel!
-    @IBOutlet var currentQuestionLabelCenterXConstraint: NSLayoutConstraint!
     
     @IBOutlet var nextQuestionLabel: UILabel!
     @IBOutlet var nextQuestionLabelCenterXConstraint: NSLayoutConstraint!
     
+    @IBOutlet var currentQuestionLabel: UILabel!
+    @IBOutlet var currentQuestionLabelCenterXConstraint: NSLayoutConstraint!
+    
     @IBOutlet var answerLabel: UILabel!
     
-    let questions: [String] = ["From what is cognac made?",
-                               "What is 7+7?",
-                               "What is the captial of Vermont?"]
+    let questions = ["From what is cognac made?",
+                     "What is 7+7?",
+                     "What is the capital of Vermont?"]
+    let answers = ["Grapes",
+                   "14",
+                   "Montpelier"]
     
-    let answers: [String] = ["Grapes",
-                             "14",
-                             "Montpelier"]
+    var currentQuestionIndex = 0
     
-    var currentQuestionIndex: Int = 0
-    
-    @IBAction func showNextQuestion(sender: AnyObject) {
-        currentQuestionIndex += 1
-        if currentQuestionIndex == questions.count {
-            currentQuestionIndex = 0
-        }
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        let question: String = questions[currentQuestionIndex]
-        currentQuestionLabel.text = question
-        answerLabel.text = "???"
-        
-        animateLabelTransitions()
+        currentQuestionLabel.text = questions[currentQuestionIndex]
+        updateOffScreenLabel()
     }
     
-    @IBAction func showAnswer(sender: AnyObject) {
-        let answer: String = answers[currentQuestionIndex]
-        answerLabel.text = answer
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Set the label's initial alpha
+        nextQuestionLabel.alpha = 0
     }
     
     func animateLabelTransitions() {
@@ -55,40 +51,42 @@ class ViewController: UIViewController {
         
         UIView.animateWithDuration(0.5,
                                    delay: 0,
-                                   options: [],
+                                   options: [.CurveLinear],
                                    animations: {
                                     self.currentQuestionLabel.alpha = 0
                                     self.nextQuestionLabel.alpha = 1
                                     
                                     self.view.layoutIfNeeded()
-                                    },
+            },
                                    completion: { _ in
-                                    swap(&self.currentQuestionLabel,
-                                         &self.nextQuestionLabel)
-                                    swap(&self.currentQuestionLabelCenterXConstraint,
-                                         &self.nextQuestionLabelCenterXConstraint)
+                                    swap(&self.currentQuestionLabel, &self.nextQuestionLabel)
+                                    swap(&self.currentQuestionLabelCenterXConstraint, &self.nextQuestionLabelCenterXConstraint)
                                     
                                     self.updateOffScreenLabel()
         })
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        // Set the label's inital alpha
-        nextQuestionLabel.alpha = 0
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let question = questions[currentQuestionIndex]
-        currentQuestionLabel.text = question
-        
-        updateOffScreenLabel()
     }
     
     func updateOffScreenLabel() {
         let screenWidth = view.frame.width
         nextQuestionLabelCenterXConstraint.constant = -screenWidth
     }
-
+    
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question = questions[currentQuestionIndex]
+        nextQuestionLabel.text = question
+        answerLabel.text = "???"
+        
+        animateLabelTransitions()
+    }
+    
+    @IBAction func showAnswer(sender: AnyObject) {
+        let answer = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
 }
+
